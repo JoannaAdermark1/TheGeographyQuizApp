b59fc2ea638797b14b9d0cc41a5e7df518ae20f0
Joanna Adermark <joanna.adermark@tutanota.com>
2024-01-31 20:56:49 +0100

    docs: add quiz.js to main

---
+++ assets/script/quiz.js
@@ -0,0 +1,120 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What is the largest country in the world?",
+    answer: "Russia",
+    options: [
+      "Sweden",
+      "Russia",
+      "China",
+      "India"
+    ]
+  },
+
+  {
+    numb: 2,
+    question: "Which of the following country is found in East Africa?",
+    answer: "Uganda",
+    options: [
+      "Benin",
+      "Togo",
+      "Uganda",
+      "Namibia"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which continent is Sweden in?",
+    answer: "Europe",
+    options: [
+      "Antarctica",
+      "Asia",
+      "Ireland",
+      "Europe"
+    ]
+  },
+
+  {
+    numb: 4,
+    question: "Which of the following is a Scandinavia country?",
+    answer: "Norway",
+    options: [
+      "Germany",
+      "Norway",
+      "Iceland",
+      "Netherland"
+    ]
+  },
+
+  {
+    numb: 5,
+    question: "Which continent is Chile",
+    answer: "South America",
+    options: [
+      "North America",
+      "America",
+      "South America",
+      "Canada"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the Capital City of Uganda?",
+    answer: "Kampala",
+    options: [
+      "Naivasha",
+      "Pretoria",
+      "Kigali",
+      "Kampala"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Whats the capital of Australia",
+    answer: "Canberra",
+    options: [
+      "Canberra",
+      "Stockholm",
+      "Sydney",
+      "Melbourne"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "what the name of the ocean between Australia and Africa?",
+    answer: "Indian Ocean",
+    options: [
+      "Atlantic Ocean",
+      "Indian Ocean",
+      "Pacific Ocean",
+      "Arctic Ocean"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Where is mount Kilimanjaro located?",
+    answer: "Tanzania",
+    options: [
+      "Kenya",
+      "Tanzania",
+      "Uganda",
+      "Rwanda"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "what is the capital of UAE?",
+    answer: "Abu Dhabi",
+    options: [
+      "Dubai",
+      "Abu Dhabi",
+      "Doha",
+      "Riyadh"
+    ]
+  },
+
+]
